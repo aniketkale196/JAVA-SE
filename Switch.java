c4f8aaa6f3404a6eea6b6b6b7b0c5452af34fb45
aniketkale196 <54441021+aniketkale196@users.noreply.github.com>
2019-09-09 16:40:55 +0530

    Add files via upload

--- Switch.java
+++ Switch.java
@@ -1,86 +1,56 @@
 /**
  * 
  */
-package assign;
+package Devpack;
 
 /**
  * @author vita1
  *
  */
 public class Switch {
-
-  private char ch ;
-
-  
-	/**
- * @param ch
- */
-public Switch(char ch) {
 	
-	this.ch = ch;
-}
-
-
-	/**
-	 * 
-	 */
-	public Switch() {
-		
-		
+	char ch;
+	public char getCh() {
+		return ch;
 	}
 
-
-	/**
- * @return the ch
- */
-public char getCh() {
-	return ch;
-}
-
-
-/**
- * @param ch the ch to set
- */
-public void setCh(char ch) {
-	this.ch = ch;
-}
-
-public void Check()
-
-{ 
-	switch(ch)
+	public void setCh(char ch) {
+		this.ch = ch;
+		
+	}
+	public void check()
 	{
-	case 'a': 
-		System.out.println("Vowel");
-		break;
-	case 'e': 
-		System.out.println("Vowel");
-		break;
-	case 'i': 
-		System.out.println("Vowel");
-		break;
-	case 'o': 
-		System.out.println("Vowel");
-		break;
-	case 'u': 
-		System.out.println("Vowel");
-		break;
-	default :
-		System.out.println("NOT Vowelllll");
-	}
-}
+		switch(ch)
+		{
+		case 'a':
+			System.out.println("Vowel");
+			break;
+		case 'e':
+			System.out.println("Vowel");
+			break;
+		case 'i':
+			System.out.println("Vowel");
+			break;
+		case 'o':
+			System.out.println("Vowel");
+			break;
+		case 'u':
+			System.out.println("Vowel");
+			break;
+		default :
+			System.out.println("Not Vowel");
+			
+		}
+			
+		}
+	
 
 	public static void main(String[] args) {
-		Switch s1 =new Switch();
-		s1.setCh('a');
-		s1.Check();
-		Switch s3= s1;
-		s3.Check();
-		Switch s2 =new Switch('q');
-		s2.Check();
-		
+		Switch s = new Switch();
+		Switch s1 = new Switch();
+		s.setCh('a');
+		s.check();
+		s1.check();
 	}
 
 }
-
-
